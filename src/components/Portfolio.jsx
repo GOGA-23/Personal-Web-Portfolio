ebdf524608b5da158f940902e12815e639e9c779
Gokul Gandhi <gokulgandhi97@gmail.com>
2023-03-01 11:34:54 +0530

    updated

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -19,7 +19,7 @@ const Portfolio = () => {
     {
       id: 2,
       src: TodoList,
-      liveDemo: "https://goga-23.github.io/TODO-List",
+      liveDemo: "https://goga-23.github.io/TODO-List-",
       sourceCode: "https://github.com/GOGA-23/TODO-List-",
     },
     {
